1d6d48ff7fce3502d17cb23e4c5916fde7d4cd5c
Christian Madajski <cmad7317@tutanota.com>
2022-02-02 09:06:02 -0500

    create initial app files

---
+++ main.py
@@ -0,0 +1,13 @@
+# generic grade calculator to make grade calculations easier
+
+def main():
+    # determine number of grade categories
+    numCategories = input('Enter number of grading categories: ')
+
+    # determine each categories grade percentage
+    percentString = input('Enter category names (NO SPACES WITHIN CATEGORIES): ')
+
+    
+
+if __name__ == '__main__':
+    main()
